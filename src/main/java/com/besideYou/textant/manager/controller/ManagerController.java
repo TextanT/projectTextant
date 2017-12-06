d66510a35ee3d351e81fe00e3397b0706113f89e
prokrammer <kooch418@gmail.com>
2017-12-06 16:03:44 +0900

    V2.0.3

---
+++ src/main/java/com/besideYou/textant/manager/controller/ManagerController.java
@@ -0,0 +1,18 @@
+package com.besideYou.textant.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ManagerController {
+
+
+//	@Autowired
+//	SignService signService;
+	
+	@RequestMapping(value = "/test.text")
+	public String test(String fileName, Model model, String bookType) throws Exception {
+		return "viewer/test";
+	}
+}
